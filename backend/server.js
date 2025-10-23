1dd2847887c1f7dfc9a5534f1a694c734f804eeb
vinh221576 <vinh221576@student.nctu.edu.vn>
2025-10-23 12:03:51 +0700

    Edir file server.js - package.json - package-lock.json

--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,12 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const userRoutes = require('./routes/user');
+const app = express();
+
 app.use('/users', userRoutes);
 
 dotenv.config();
-const app = express();
+
 app.use(cors()); // Cho phép frontend gọi API
 app.use(express.json()); // Parse body JSON
 
