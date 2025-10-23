19653d071244baa91c60b89f9da244530cb67712
vinh221576 <vinh221576@student.nctu.edu.vn>
2025-10-23 21:00:53 +0700

    Sửa đường dẫn, cập nhật get-put, cập nhật get-delete cho admin

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
+const { authMiddleware } = require('../middleware/auth');
 
-router.get('/users', userController.getUsers);
-router.post('/users', userController.createUser);
+router.post('/signup', userController.signup);
+router.post('/login', userController.login);
+router.get('/logout', userController.logout); // Optional
+//Them chuc nang lay va cap nhat profile
+router.get('/profile', authMiddleware, userController.getProfile);
+router.put('/profile', authMiddleware, userController.updateProfile);
 
-// Thêm PUT và DELETE
-router.put('/users/:id', userController.updateUser);
-router.delete('/users/:id', userController.deleteUser);
+// Chcứ năng ấy danh sách và xóa người dùng của Admin routes
+router.get('/users', authMiddleware, adminMiddleware, userController.getUsers);
+router.delete('/users/:id', authMiddleware, adminMiddleware, userController.deleteUser);
 module.exports = router;
