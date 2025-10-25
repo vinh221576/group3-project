d38334ae70f83e588f2902bd32d0eab743b35aba
vinh221576 <vinh221576@student.nctu.edu.vn>
2025-10-25 21:15:49 +0700

    Thêm chức năng tự động xóa (deleteself) cho user(role) vào routes/user.js

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
+
 // SỬA DÒNG NÀY: Giờ đã lấy cả authMiddleware và adminMiddleware
 const { authMiddleware, adminMiddleware } = require('../middleware/auth'); 
 
@@ -11,9 +12,11 @@ router.get('/logout', userController.logout); // Optional
 // Them chuc nang lay va cap nhat profile
 router.get('/profile', authMiddleware, userController.getProfile);
 router.put('/profile', authMiddleware, userController.updateProfile);
+//Thêm chức năng xóa chính mình cho user(role)
+router.delete('/profile', authMiddleware, userController.deleteSelf); 
 
 // Chcứ năng ấy danh sách và xóa người dùng của Admin routes
-router.get('/users', authMiddleware, adminMiddleware, userController.getUsers);
-router.delete('/users/:id', authMiddleware, adminMiddleware, userController.deleteUser);
+router.get('/', authMiddleware, adminMiddleware, userController.getUsers);
+router.delete('/:id', authMiddleware, adminMiddleware, userController.deleteUser);
 
 module.exports = router;
